a09bd71e9e4447bd2f2283ae78d9aa27fbe92459
Phi Thai <thaimanhphi@gmail.com>
2023-04-14 14:13:12 +0700

    Initial Commit

---
+++ Kitty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kitty
+//
+//  Created by phi.thai on 4/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
