857401446cee8989326cda74f7663757c9841bdd
Phi Thai <thaimanhphi@gmail.com>
2023-06-23 13:24:38 +0700

    clean up

--- Kitty/View/Component/TabBar View/TabBarViewController.swift
+++ Kitty/View/Component/TabBar View/TabBarViewController.swift
@@ -18,17 +18,5 @@ class TabBarViewController: UITabBarController {
 
         // Do any additional setup after loading the view.
         self.selectedIndex = 1
-        
-        
-        let firstVC = self.viewControllers![0] as! ReportViewController //first view controller in the tabbar
-//        firstVC.set(items: viewModel.getAllItems(), history: viewModel.getAllHistory(), filteredMonth: viewModel.getCurrentMonth())
-
-        let secondVC = self.viewControllers![1] as! HomeViewController //first view controller in the tabbar
-//        secondVC.set(items: viewModel.getAllItems(), history: viewModel.getAllHistory(), income: viewModel.getIncome(), iconArray: viewModel.getAllIcon(), month: viewModel.getAllMonth(), filteredMonth: viewModel.getCurrentMonth())
-//        
-        _ = self.viewControllers![2] as! SettingsViewController //first view controller in the tabbar
-
-        
-        
     }
 }
